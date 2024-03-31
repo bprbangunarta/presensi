5d75980b1b8857b95fdea0a012c9b19f1e2888dd
Zulfadli Rizal <dev@bprbangunarta.co.id>
2024-04-01 04:35:05 +0700

    Fix

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -3,14 +3,17 @@
         <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ms-lg-auto">
                 <ul class="list-inline list-inline-dots mb-0">
-                    <li class="list-inline-item"><b>Presensi v1.0</b></li>
+                    <li class="list-inline-item"><b>Attendance v1.0</b></li>
                 </ul>
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0">
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
-                        Copyright &copy; 2023
-                        <a href="https://bprbangunarta.co.id" class="link-secondary">BPR Bangunarta</a>
+                        Copyright &copy;
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        <a href="https://bprbangunarta.co.id" class="link-secondary">Ninja Epress</a>
                     </li>
                 </ul>
             </div>
